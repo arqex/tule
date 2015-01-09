d1eee1ea77029af3c187dd8a89d7dc416e69de99
Javi <javi@arqex.com>
2015-01-09 13:52:31 +0100

    Improved string datatype to show all the content when editing

--- public/modules/datatypes/string/stringType.js
+++ public/modules/datatypes/string/stringType.js
@@ -10,7 +10,16 @@ define(deps, function($,_,Backbone, tplSource, DatatypeViews){
 
 	var StringTypeView = DatatypeViews.DataTypeView.extend({
 		displayTpl: _.template($(tplSource).find('#stringTpl').html()),
-		editTpl: _.template($(tplSource).find('#stringEditTpl').html())
+		editTpl: _.template($(tplSource).find('#stringEditTpl').html()),
+		save: function(e) {
+			var input = this.$('input');
+
+			if(!input.length)
+				input = this.$('textarea');
+
+			this.model.set('value', input.val());
+			this.trigger('edit:ok', input.val());
+		},
 	});
 
 	return {
